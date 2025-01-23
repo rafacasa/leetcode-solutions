4bf7e8ddaf89c4e3280b980a324c97c3b226d73f
Rafael Luiz Casa <rafael.casa@pm.me>
2025-01-22 22:20:26 -0300

    Submission question 1

---
+++ 1_two_sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, num1 in enumerate(nums):
+            for j, num2 in enumerate(nums):
+                if i == j:
+                    continue
+                if (num1 + num2) == target:
+                    return [i, j]
