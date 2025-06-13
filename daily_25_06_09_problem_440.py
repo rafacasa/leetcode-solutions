821764e565382c5d339b077e02e037c1026158bb
Rafael Luiz Casa <rafael.casa@pm.me>
2025-06-13 18:52:06 -0300

    daily from june 9th

--- daily_25_06_09_problem_440.py
+++ daily_25_06_09_problem_440.py
@@ -1,15 +1,38 @@
-from typing import List
-
-# Memory Limit Exceeded
+# Accepted
+# Runtime 0 ms Beats 100.00%
+# Memory 17.83 MB Beats 35.88%
 
 
 class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
-        ans = [str(i) for i in range(1, n + 1)]
-        return sorted(ans)
+    def get_cnt_edges(self, start: int, n: int) -> int:
+        cnt = 0
+        left = start * 10
+        right = left + 9
+
+        while right <= n:
+            cnt += right - left + 1
+            left *= 10
+            right = right * 10 + 9
+
+        if left <= n:
+            cnt += n - left + 1
+
+        return cnt
 
     def findKthNumber(self, n: int, k: int) -> int:
-        return int(self.lexicalOrder(n)[k - 1])
+        n_step = 1
+        k_step = 1
+
+        while k_step != k:
+            cnt = self.get_cnt_edges(n_step, n)
+            if k_step + cnt < k:
+                n_step += 1
+                k_step += cnt + 1
+            else:
+                n_step *= 10
+                k_step += 1
+
+        return n_step
 
 
 print(Solution().findKthNumber(13, 2))
